ce2a3e9ae5ec7a61617b0b5d7a9287859aa64427
Ben Hooper <ben@labrys.io>
2025-04-13 21:25:35 +1000

    feat: got front end calling the backend

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,4 @@
-use dioxus::{logger, prelude::*};
+use dioxus::{logger::tracing, prelude::*};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -18,22 +18,40 @@ fn App() -> Element {
     }
 }
 
+#[derive(serde::Deserialize)]
+struct UniV2API {
+    pool: String,
+    src: String,
+    dst: String,
+    amount_out: String,
+}
+
 #[component]
 pub fn SwapForm() -> Element {
-    let mut pool_address = use_signal(|| "".to_string());
-    let mut token_in = use_signal(|| String::new());
-    let mut token_out = use_signal(|| String::new());
-    let mut amount_in = use_signal(|| String::new());
+    let mut pool_address = use_signal(|| "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852".to_string());
+    let mut token_in = use_signal(|| "0xdAC17F958D2ee523a2206206994597C13D831ec7".to_string());
+    let mut token_out = use_signal(|| "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2".to_string());
+    let mut amount_in = use_signal(|| "10000000".to_string());
 
-    let get_quote = move |_| {
+    let get_quote = move |_| async move {
         let url = format!(
-            "http://localhost:1337?pool_address={}&token_in={}&token_out={}&amount_in={}",
+            "https://uni-v2.hooper.link/estimate?pool={}&src={}&dst={}&src_amount={}",
             pool_address, token_in, token_out, amount_in
         );
 
         // In a real app, you would make the HTTP request here using reqwest or gloo-net
         // For now, we'll just log the URL
-        println!("Getting quote from: {}", url);
+        tracing::info!("Rendering app!");
+        tracing::info!("{}", url);
+
+        let response = reqwest::get(url)
+            .await
+            .unwrap()
+            .json::<UniV2API>()
+            .await
+            .unwrap();
+
+        tracing::info!("{}", response.amount_out);
 
         // Placeholder for the actual HTTP request:
         // use_future(async move {
