2df1c7f30701c11912f2c2361c5d87321a7c9a4f
Ben Hooper <ben@labrys.io>
2025-04-13 20:50:57 +1000

    feat: checkpointing simple front end

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,7 @@
-use dioxus::prelude::*;
+use dioxus::{logger, prelude::*};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
@@ -26,15 +25,61 @@ pub fn SwapForm() -> Element {
     let mut token_out = use_signal(|| String::new());
     let mut amount_in = use_signal(|| String::new());
 
-    let mut name = use_signal(|| "bob".to_string());
+    let get_quote = move |_| {
+        let url = format!(
+            "http://localhost:1337?pool_address={}&token_in={}&token_out={}&amount_in={}",
+            pool_address, token_in, token_out, amount_in
+        );
+
+        // In a real app, you would make the HTTP request here using reqwest or gloo-net
+        // For now, we'll just log the URL
+        println!("Getting quote from: {}", url);
+
+        // Placeholder for the actual HTTP request:
+        // use_future(async move {
+        //     let resp = reqwest::get(&url).await?;
+        //     let text = resp.text().await?;
+        //     Ok::<_, reqwest::Error>(text)
+        // });
+    };
 
     rsx! {
-        input {
-            class: "text-black",
-            // we tell the component what to render
-            value: "{name}",
-            // and what to do when the value changes
-            oninput: move |event| name.set(event.value())
+        div { class: "flex flex-col gap-4 p-4 max-w-md mx-auto",
+            h2 { class: "text-xl font-bold mb-4", "Swap Form" }
+
+            label { "Pool Address:" }
+            input {
+                class: "text-black p-2 border rounded",
+                value: "{pool_address}",
+                oninput: move |event| pool_address.set(event.value())
+            }
+
+            label { "Token In:" }
+            input {
+                class: "text-black p-2 border rounded",
+                value: "{token_in}",
+                oninput: move |event| token_in.set(event.value())
+            }
+
+            label { "Token Out:" }
+            input {
+                class: "text-black p-2 border rounded",
+                value: "{token_out}",
+                oninput: move |event| token_out.set(event.value())
+            }
+
+            label { "Amount In:" }
+            input {
+                class: "text-black p-2 border rounded",
+                value: "{amount_in}",
+                oninput: move |event| amount_in.set(event.value())
+            }
+
+            button {
+                class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4",
+                onclick: get_quote,
+                "Get Quote"
+            }
         }
     }
 }
